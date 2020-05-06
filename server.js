db21f4f8ff05b862cc1a9c4904559cbc7a7a2e58
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-05-05 22:33:48 -0500

    added connect-timeout package

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
-
+var timeout = require("connect-timeout");
 var PORT = process.env.PORT || 8080;
-
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
