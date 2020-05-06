6ad941e265b308722ec1b7416ef8c9ea9a62d778
Timothy Mickiewicz <timothy.mickiewicz@gmail.com>
2020-05-05 22:18:35 -0500

    added timeout function

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ var routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
+// Timeout
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+    if (!req.timedout) next();
+}
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
